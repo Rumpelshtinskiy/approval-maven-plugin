46b3e1535f38a007e125f90865b301524a8bf81f
Rustam Galikhanov <galikhanovrustam@gmail.com>
2024-11-22 10:34:12 +0500

    Chage version

--- src/main/java/org/rugal/util/ErrorMap.java
+++ src/main/java/org/rugal/util/ErrorMap.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 @UtilityClass
 public class ErrorMap {
-    private final String START_DIRECTORY_PATH = "start_directory_path";
+    private static final String START_DIRECTORY_PATH = "start_directory_path";
 
     private final Map<String, String> MAP = Map.of("Configuration error: You must configure at least one set of arguments for this @ParameterizedTest", "Didn't found any target files in start_directory_path=%s");
     public String substituteError(String error) {
